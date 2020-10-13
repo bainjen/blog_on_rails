ea121f6ed98f16b23f45a0c0d2650210711d88f4
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-13 23:14:15 +0000

    implement update and patch funcitonality, add edit and back links to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,15 +12,29 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-def create
-  @article = Article.new(article_params)
- 
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+  def edit
+    @article = Article.find(params[:id])
   end
-end
+
+  def create
+    @article = Article.new(article_params)
+  
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
    
   private
     def article_params
