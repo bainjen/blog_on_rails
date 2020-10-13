64ff7bb13654e2d72786cd7e168478fde76d8ddc
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-13 22:40:49 +0000

    add validation logic to ensure user submits a title and that it is at least 3 characters long

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,15 +8,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
  
-  def new 
+  def new
+    @article = Article.new
   end
 
-  def create
-    @article = Article.new(article_params)
-   
-    @article.save
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
     redirect_to @article
+  else
+    render 'new'
   end
+end
    
   private
     def article_params
